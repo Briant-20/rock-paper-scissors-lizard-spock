50fc5b2e6e082884c1a1143f22fadb70e03c02ab
Brian Teahan <Brian.teahan@mycit.ie>
2023-04-08 11:42:17 +0100

    Make a function that gets the computers choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ let winner;
 let playerWins = 0;
 let computerWins = 0;
 let tieGames = 0;
+let choices = ["Rock","Paper","Scissors","Lizard","Spock"];
+
+getComputerChoice = () => {
+    let num = Math.floor(Math.random() * 5);
+    computerChoice = choices[num];
+    }
