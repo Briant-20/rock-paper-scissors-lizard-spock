555940ce86a93a2f216130bf9faa4186d302d863
Brian Teahan <Brian.teahan@mycit.ie>
2023-04-10 14:40:22 +0100

    Add a function that gets users choice on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let computerChoice;
+var playerChoice;
 let winner;
 let playerWins = 0;
 let computerWins = 0;
@@ -9,3 +10,17 @@ getComputerChoice = () => {
     let num = Math.floor(Math.random() * 5);
     computerChoice = choices[num];
     }
+
+getPlayerChoice = (event) => {
+    playerChoice = dataType;
+    console.log(playerChoice);
+}
+
+let buttons = document.getElementsByTagName("button");
+for (let i = 0; i < buttons.length; i++) {
+    getDataType = (event) => {
+        dataType = buttons[i].getAttribute("data-type")
+    }
+    buttons[i].addEventListener("click", getDataType);
+    buttons[i].addEventListener("click", getPlayerChoice);
+}
