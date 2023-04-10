1a8ef120844fa6ed77a0a32254364b330f29bbab
Brian Teahan <Brian.teahan@mycit.ie>
2023-04-10 18:19:34 +0100

    Add a function that loops the game three times

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ let winner;
 let playerWins = 0;
 let computerWins = 0;
 let tieGames = 0;
+let games = 0;
+let buttons = document.getElementsByTagName("button");
 let choices = ["rock","paper","scissors","lizard","spock"];
 let rules = {
     rock: { 
@@ -14,40 +16,57 @@ let rules = {
     },
     scissors: {
          beats: ['paper','lizard'] 
-        },
+    },
     lizard: {
          beats: ['spock','paper'] 
-        },
+    },
     spock: {
          beats: ['scissors','rock'] 
-        }
-  }
+    }
+}
 
 getComputerChoice = () => {
     let num = Math.floor(Math.random() * 5);
     computerChoice = choices[num];
     return computerChoice;
-    }
+}
 
-playRound = (event) => {
+playRound = () => {
+    if (games < 3){
     computerChoice = getComputerChoice();
     console.log(`The computer chose ${computerChoice}`);
     console.log(`You chose ${playerChoice}`);
     if(rules[playerChoice].beats.includes(computerChoice)){
         console.log("You win this round!");
+        playerWins++;
+        games++
     }
-    if(rules[computerChoice].beats.includes(playerChoice)){
+    else if(rules[computerChoice].beats.includes(playerChoice)){
         console.log("Sorry the computer wins this round!");
+        computerWins++;
+        games++
     }
-    if(playerChoice === computerChoice){
+    else{
         console.log("This round is a tie try again");
     }
+    if (games === 3){
+        console.log(games);
+        if (computerWins > playerWins){
+            console.log(`The computer wins the game with ${computerWins} rounds total out of 3`);
+            }
+        else if (playerWins > computerWins){
+                console.log(`Congrtulations you win the game with ${playerWins} rounds total out of 3`);
+        }
+    }
 }
-let buttons = document.getElementsByTagName("button");
-for (let i = 0; i < buttons.length; i++) {
-    getPlayerChoice = (event) => {
+}
+playGame = () => {
+    for (let i = 0; i < buttons.length; i++) {
+        getPlayerChoice = (event) => {
         playerChoice = buttons[i].getAttribute("choice");
     }
-    buttons[i].addEventListener("click", getPlayerChoice);
-    buttons[i].addEventListener("click", playRound);
-}
+        buttons[i].addEventListener("click", getPlayerChoice);
+        buttons[i].addEventListener("click", playRound);
+    }
+    }
+playGame();
