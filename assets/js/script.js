473d24e6120dffd31caa27640cfc6a09560eb5d6
Brian Teahan <Brian.teahan@mycit.ie>
2023-04-12 13:55:40 +0100

    Remove excess code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,15 @@ let computerWins = 0;
 let tieGames = 0;
 let games = 0;
 let buttons = document.getElementsByTagName("button");
+let startGame = document.getElementById("play-again");
 let playAgain = document.getElementsByTagName("p")[0];
-let playAgainBox = document.querySelector('#play-again');
-let playAgainButton = playAgainBox.querySelector('button');
-playAgainBox.style.display = 'none';
+startGame.style.display = 'none';
 let choices = ["rock","paper","scissors","lizard","spock"];
 let rules = {
-    rock: { 
+    rock: {
         beats: ['scissors','lizard'] 
     },
-    paper: { 
+    paper: {
         beats: ['rock','spock'] 
     },
     scissors: {
@@ -37,48 +36,49 @@ getComputerChoice = () => {
 
 playRound = () => {
     if (games < 3){
-    computerChoice = getComputerChoice();
-    console.log(`The computer chose ${computerChoice}`);
-    console.log(`You chose ${playerChoice}`);
-    if(rules[playerChoice].beats.includes(computerChoice)){
-        console.log("You win this round!");
-        playerWins++;
-        games++
-    }
-    else if(rules[computerChoice].beats.includes(playerChoice)){
-        console.log("Sorry the computer wins this round!");
-        computerWins++;
-        games++
-    }
-    else{
-        console.log("This round is a tie try again");
-    }
-    if (games === 3){
-        console.log(games);
-        if (computerWins > playerWins){
-            playAgainBox.style.display = 'block';
-            playAgain.innerHTML = `The computer wins the game with ${computerWins} rounds total out of 3`;
+        computerChoice = getComputerChoice();
+        console.log(`The computer chose ${computerChoice}`);
+        console.log(`You chose ${playerChoice}`);
+        if(rules[playerChoice].beats.includes(computerChoice)){
+            console.log("You win this round!");
+            playerWins++;
+            games++
+        }
+        else if(rules[computerChoice].beats.includes(playerChoice)){
+            console.log("Sorry the computer wins this round!");
+            computerWins++;
+            games++
+        }
+        else{
+            console.log("This round is a tie try again");
+        }
+        if (games === 3){
+            console.log(games);
+            if (computerWins > playerWins){
+                startGame.style.display = 'block';
+                playAgain.innerHTML = `The computer wins the game with ${computerWins} rounds total out of 3`;
+                }
+            else if (playerWins > computerWins){
+                startGame.style.display = 'block';
+                playAgain.innerHTML = `Congrtulations you win the game with ${playerWins} rounds total out of 3`;
             }
-        else if (playerWins > computerWins){
-            playAgainBox.style.display = 'block';
-            playAgain.innerHTML = `Congrtulations you win the game with ${playerWins} rounds total out of 3`;
         }
     }
 }
-}
-playGame = (event) => {
+
+playGame = () => {
     games = 0;
     computerWins = 0;
     playerWins = 0;
-    playAgainBox.style.display = 'none';
+    startGame.style.display = 'none';
     for (let i = 0; i < buttons.length; i++) {
         getPlayerChoice = (event) => {
         playerChoice = buttons[i].getAttribute("choice");
-    }
+        }
         buttons[i].addEventListener("click", getPlayerChoice);
         buttons[i].addEventListener("click", playRound);
     }
-    }
+}
+
 playGame();
-let startGame = document.getElementById("play-again");
 startGame.addEventListener("click", playGame);
