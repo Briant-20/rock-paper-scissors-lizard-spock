8150ba7974022ba998f78694c9a7e39d0e1d66aa
Brian Teahan <Brian.teahan@mycit.ie>
2023-04-15 13:46:28 +0100

    Add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Initialize variables for game state
 let computerChoice;
 let playerChoice;
 let winner;
@@ -5,6 +6,7 @@ let playerWins = 0;
 let computerWins = 0;
 let tieGames = 0;
 let games = 0;
+// Get HTML elements for variables
 let buttons = document.getElementsByTagName("button");
 let playAgain = document.getElementById("play-again");
 let playAgainText = document.getElementsByTagName("p")[3];
@@ -16,7 +18,9 @@ let choices = ["rock","paper","scissors","lizard","spock"]
 let playerIcon = document.getElementsByTagName("p")[1];
 let computerIcon = document.getElementsByTagName("p")[2];
 let playerChoiceIcon;
+// Hide the play-again button initially
 playAgain.style.display = 'none';
+// Define rules for which choices beat which other choices
 let rules = {
     rock: {
         beats: ['scissors','lizard'] 
@@ -34,7 +38,7 @@ let rules = {
          beats: ['scissors','rock'] 
     }
 }
-
+// Function to randomly select computer's choice
 getComputerChoice = () => {
     let num = Math.floor(Math.random() * 5);
     computerChoice = choices[num];
@@ -42,13 +46,18 @@ getComputerChoice = () => {
     computerChoiceIcon = computerChoiceIcon[num].innerHTML;
 }
 
+// Function to play a round of the game
 playRound = () => {
+    // Only play a round if less than 3 rounds have been played
     if (games < 3){
+        // Get the computer's choice and the player's choice
         getComputerChoice();
         playerIcon.innerHTML = playerChoiceIcon;
         computerIcon.innerHTML = computerChoiceIcon;
+        // Display the round results
         roundResults.style.display = 'flex';
         choiceIcons.style.display = 'flex';
+        // Determine the winner of the round based on the rules
         if(rules[playerChoice].beats.includes(computerChoice)){
             roundResults.innerHTML = "You win this round! ";
             roundResults.innerHTML += `${playerChoice} beats ${computerChoice}`
@@ -64,6 +73,7 @@ playRound = () => {
         else{
             roundResults.innerHTML = "This round is a tie try again";
         }
+        // If three rounds have been played, end the game
         if (games === 3){
             gameArea.style.display = 'none';
             playAgain.style.display = 'flex';
@@ -77,7 +87,10 @@ playRound = () => {
     }
 }
 
+
+// Function to start a new game
 playGame = () => {
+    // Reset game state
     games = 0;
     computerWins = 0;
     playerWins = 0;
@@ -85,16 +98,25 @@ playGame = () => {
     gameArea.style.display = 'block';
     choiceIcons.style.display = 'none';
     roundResults.innerHTML = "Best of 3, choose wisely";
-    for (let i = 0; i < buttons.length; i++) {
+     // Set up event listeners for each button
+     for (let i = 0; i < buttons.length; i++) {
         getPlayerChoice = () => {
-        playerChoice = buttons[i].getAttribute("choice");
-        playerChoiceIcon = gameArea.querySelectorAll("button");
-        playerChoiceIcon = playerChoiceIcon[i-1].innerHTML;
+            // Get the player's choice from the button's choice attribute
+            playerChoice = buttons[i].getAttribute("choice");
+
+            // Set the playerChoiceIcon variable to the innerHTML of the button
+            playerChoiceIcon = gameArea.querySelectorAll("button");
+            playerChoiceIcon = playerChoiceIcon[i-1].innerHTML;
         }
+
+        // Add event listeners to the button
         buttons[i].addEventListener("click", getPlayerChoice);
         buttons[i].addEventListener("click", playRound);
     }
 }
 
+// Call the playGame function to start the game
 playGame();
+
+// Add an event listener to the playAgainButton to start a new game when clicked
 playAgainButton.addEventListener("click", playGame);
